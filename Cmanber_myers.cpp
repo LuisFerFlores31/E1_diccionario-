d7e971fd7988d78e3ba42c76456ba4864ac270e7
Luis Fernando Flores <a01738347@tec.mx>
2025-10-06 16:25:32 -0600

    Fixed std

--- Cmanber_myers.cpp
+++ Cmanber_myers.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+using namespace std;
 
 struct SubstrRank {
     int left_rank;
@@ -10,8 +11,8 @@ struct SubstrRank {
     SubstrRank(int l, int r, int i) : left_rank(l), right_rank(r), index(i) {}
 };
 
-std::vector<int> make_ranks(const std::vector<SubstrRank>& substr_rank, int n) {
-    std::vector<int> rank(n, -1);
+vector<int> make_ranks(const vector<SubstrRank>& substr_rank, int n) {
+    vector<int> rank(n, -1);
     int r = 0;
     rank[substr_rank[0].index] = r;
     for (int i = 1; i < n; ++i) {
@@ -24,34 +25,34 @@ std::vector<int> make_ranks(const std::vector<SubstrRank>& substr_rank, int n) {
     return rank;
 }
 
-std::vector<int> suffix_array(const std::string& T) {
+vector<int> suffix_array(const string& T) {
     int n = T.size();
-    std::vector<SubstrRank> substr_rank;
+    vector<SubstrRank> substr_rank;
     for (int i = 0; i < n; ++i) {
         int left = T[i];
         int right = (i < n-1) ? T[i+1] : 0;
         substr_rank.emplace_back(left, right, i);
     }
 
-    std::sort(substr_rank.begin(), substr_rank.end(), [](const SubstrRank& a, const SubstrRank& b) {
-        return std::tie(a.left_rank, a.right_rank) < std::tie(b.left_rank, b.right_rank);
+    sort(substr_rank.begin(), substr_rank.end(), [](const SubstrRank& a, const SubstrRank& b) {
+        return tie(a.left_rank, a.right_rank) < tie(b.left_rank, b.right_rank);
     });
 
     int l = 2;
     while (l < n) {
-        std::vector<int> rank = make_ranks(substr_rank, n);
+        vector<int> rank = make_ranks(substr_rank, n);
         for (int i = 0; i < n; ++i) {
             substr_rank[i].left_rank = rank[i];
             substr_rank[i].right_rank = (i + l < n) ? rank[i + l] : 0;
             substr_rank[i].index = i;
         }
-        std::sort(substr_rank.begin(), substr_rank.end(), [](const SubstrRank& a, const SubstrRank& b) {
-            return std::tie(a.left_rank, a.right_rank) < std::tie(b.left_rank, b.right_rank);
+        sort(substr_rank.begin(), substr_rank.end(), [](const SubstrRank& a, const SubstrRank& b) {
+            return tie(a.left_rank, a.right_rank) < tie(b.left_rank, b.right_rank);
         });
         l *= 2;
     }
 
-    std::vector<int> SA(n);
+    vector<int> SA(n);
     for (int i = 0; i < n; ++i) {
         SA[i] = substr_rank[i].index;
     }
@@ -59,11 +60,11 @@ std::vector<int> suffix_array(const std::string& T) {
 }
 
 int main() {
-    std::string text = "mississippi";
-    std::vector<int> SA = suffix_array(text);
+    string text = "mississippi";
+    vector<int> SA = suffix_array(text);
     for (int i : SA) {
-        std::cout << i << " ";
+        cout << i << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
     return 0;
 }
